f5a4633cc3f145a4b80de0ab0de9483b987bcc03
Drekan (Bastien S.) <segura.bastien.dev@gmail.com>
2020-10-28 18:21:10 +0100

    création des index, pas encore triés

--- src/parsers/RDFRawParser.java
+++ src/parsers/RDFRawParser.java
@@ -19,20 +19,30 @@ public final class RDFRawParser {
 		public int valeur = 0;
 		
 		public Dictionnaire dico;
+		public ArrayList<Index> indexes;
 		
-		public RDFListener(Dictionnaire d) {
+		public RDFListener(Dictionnaire d,ArrayList<Index> i) {
 			super();
 			this.dico = d;
+			this.indexes = i;
 		}
 		
 		
 		@Override
 		public void handleStatement(Statement st) {
+			String subject = st.getSubject().toString() ;
+			String predicate = st.getPredicate().toString();
+			String object = st.getObject().toString();
+			
 			//this.trace.add(st.getSubject().toString());
 			this.valeur+=3;
 			this.dico.add(st.getSubject().toString());
 			this.dico.add(st.getPredicate().toString());
 			this.dico.add(st.getObject().toString());
+			
+			for(Index index : this.indexes) {
+				index.add(subject.hashCode(),predicate.hashCode(),object.hashCode());
+			}
 
 		}
 
@@ -46,7 +56,18 @@ public final class RDFRawParser {
 		
 		Dictionnaire d = new Dictionnaire();
 		
-		RDFListener rdf_l = new RDFListener(d);
+		ArrayList<Index> indexes = new ArrayList<>();
+		
+		indexes.add(new Index("spo"));
+		indexes.add(new Index("sop"));
+		
+		indexes.add(new Index("pos"));
+		indexes.add(new Index("pso"));
+		
+		indexes.add(new Index("osp"));
+		indexes.add(new Index("ops"));
+		
+		RDFListener rdf_l = new RDFListener(d,indexes);
 		
 		
 		rdfParser.setRDFHandler(rdf_l);
@@ -56,6 +77,13 @@ public final class RDFRawParser {
 			rdfParser.parse(reader, "");
 			System.out.println("La taille du dictionnaire est de " + d.getSize());
 			System.out.println("Nombre de ressources lues : " + rdf_l.valeur);
+			
+			Index sop = indexes.get(0);
+			System.out.println("\nIndex SOP : (10 premières valeurs)");
+			for(int i=0;i<10;i++) {
+				int[] triple = sop.getTriple(i);
+				System.out.println("["+triple[0]+","+triple[1]+","+triple[2]+"]");
+			}
 
 		} catch (Exception e) {
 
