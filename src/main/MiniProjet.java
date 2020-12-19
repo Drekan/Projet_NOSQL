634464fdd41ed2cda478b2bbb05b3d08fcd25037
Llivia Langevin <llivia.langevin@etu.umontpellier.fr>
2020-12-19 16:08:59 +0100

    Commented some testing stuff

--- src/main/MiniProjet.java
+++ src/main/MiniProjet.java
@@ -30,13 +30,13 @@ public class MiniProjet {
 		//options.setExport_query_results(true);
 		//options.setExport_query_stats(true);
 		//options.setJena(true);
-
+		/*
 		options.setCheckJena(true);
 		options.setOptim_none(false);
 		options.setDiagnostic(false);
 		options.setDataPath("datasets/500K.rdfxml");
 		options.setQueriesPath("star_queries_final.txt");
-
+		*/
 		//options.setQueriesPath("general_queries_final.txt");
 		//options.setQueriesPath("query.txt");
 		//options.setStar_queries(true);
